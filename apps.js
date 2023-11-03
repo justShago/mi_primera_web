fead2cd386bb32ef2e41fc1e1caeb8290b57fb61
justShago <117535419+justShago@users.noreply.github.com>
2023-11-02 19:26:24 -0500

    Initial commit

---
+++ apps.js
@@ -0,0 +1,38 @@
+//alert("Hola este es mi Javascript");
+//let nombre = "Santiago";
+
+//let contenidoTitle = "Enygma";
+
+//let title = document.querySelector(".logo .fuente");
+//title.innerHTML = contenidoTitulo;
+
+// CONDICIONALES
+
+//if(textoTitle == "Nombre"){
+//    title.innerHTML = "Otro";
+//} else {
+//    console.log("no se cumple")
+//}
+
+//FUNCIONES
+// document.querySelector("p")
+// console.log=(document.querySelector(`.Parrafo`))
+
+// let nombre = "Zilef";
+// let producto = "Pantalones";
+// let tiponegocio = "Chuzo";
+
+// let parrafo = document.getElementById(`parrafo`);
+// // parrafo.textContent = "texto desde JS"
+// // parrafo.innerHTML = "<b>texto</b> desde JS"
+
+// function replacer = (nombre, producto, tiponegocio) {
+// let contenido = `En ${nombre}, somos más que una ${tiponegocio} en línea de ${producto} personalizadas y accesorios deportivos. Somos una comunidad de apasionados por el deporte y la moda, fusionando el estilo y la individualidad en cada prenda que creamos.`;
+
+// return replacement;
+// }
+
+const p = 'En Enygma, somos más que una tienda en línea de camisetas personalizadas y accesorios deportivos. Somos una comunidad de apasionados por el deporte y la moda, fusionando el estilo y la individualidad en cada prenda que creamos.';
+
+console.log(p.replaceAll('Enygma', 'zilef'));
+// Expected output: "The quick brown fox jumps over the lazy monkey. If the mon

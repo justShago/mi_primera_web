cb93d86099cc1001205b26bcc8e86d81883e7f35
justShago <117535419+justShago@users.noreply.github.com>
2023-11-07 19:08:50 -0500

    update

--- apps.js
+++ apps.js
@@ -1,38 +1,20 @@
-//alert("Hola este es mi Javascript");
-//let nombre = "Santiago";
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById("miFormulario");
 
-//let contenidoTitle = "Enygma";
+    formulario.addEventListener("submit", function (event) {
+        event.preventDefault(); // Evita que el formulario se envíe automáticamente
 
-//let title = document.querySelector(".logo .fuente");
-//title.innerHTML = contenidoTitulo;
+        // Obtenemos los valores de los campos
+        const nombre = document.getElementById("nombre").value;
+        const email = document.getElementById("email").value;
 
-// CONDICIONALES
-
-//if(textoTitle == "Nombre"){
-//    title.innerHTML = "Otro";
-//} else {
-//    console.log("no se cumple")
-//}
-
-//FUNCIONES
-// document.querySelector("p")
-// console.log=(document.querySelector(`.Parrafo`))
-
-// let nombre = "Zilef";
-// let producto = "Pantalones";
-// let tiponegocio = "Chuzo";
-
-// let parrafo = document.getElementById(`parrafo`);
-// // parrafo.textContent = "texto desde JS"
-// // parrafo.innerHTML = "<b>texto</b> desde JS"
-
-// function replacer = (nombre, producto, tiponegocio) {
-// let contenido = `En ${nombre}, somos más que una ${tiponegocio} en línea de ${producto} personalizadas y accesorios deportivos. Somos una comunidad de apasionados por el deporte y la moda, fusionando el estilo y la individualidad en cada prenda que creamos.`;
-
-// return replacement;
-// }
-
-const p = 'En Enygma, somos más que una tienda en línea de camisetas personalizadas y accesorios deportivos. Somos una comunidad de apasionados por el deporte y la moda, fusionando el estilo y la individualidad en cada prenda que creamos.';
-
-console.log(p.replaceAll('Enygma', 'zilef'));
-// Expected output: "The quick brown fox jumps over the lazy monkey. If the mon
+        // Realiza la validación
+        if (nombre.trim() === "" || email.trim() === "") {
+            alert("Por favor, completa todos los campos.");
+        } else {
+            // Si la validación pasa, puedes enviar el formulario o realizar otras acciones aquí
+            alert("Formulario enviado correctamente.");
+            formulario.reset(); // Opcional: Restablece el formulario después de enviarlo
+        }
+    });
+});
